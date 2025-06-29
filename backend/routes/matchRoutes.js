6a9860b25a6352f533aa6c3deff232ddae45a6c1
Saurav <114897038+Saurav-5123@users.noreply.github.com>
2025-06-29 22:16:51 +0530

    Delete backend directory

--- backend/routes/matchRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getMatches } = require('../controllers/matchController');
-const router = express.Router();
-
-router.post('/match', getMatches);
-
-module.exports = router;
