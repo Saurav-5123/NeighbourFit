56dfed56ee9a9741a67d0b34c2925e856787cbd9
Saurav <114897038+Saurav-5123@users.noreply.github.com>
2025-06-29 22:19:21 +0530

    Delete neighborfit-frontend directory

--- neighborfit-frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <p>&copy; 2025 NeighborFit. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- neighborfit-frontend/src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <section className="hero" id="home">
-      <h1>Find Your Ideal Neighborhood</h1>
-    </section>
-  );
-};
-
-export default Home;
